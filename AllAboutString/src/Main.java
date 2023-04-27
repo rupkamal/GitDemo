14ac96e7af46706305970b359e48d8a452890f06
rupguha <rupkamaldgp@gmail.com>
2023-04-27 13:41:43 -0300

    First Commit

---
+++ AllAboutString/src/Main.java
@@ -0,0 +1,37 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		printInformation("Hello World");
+		/*
+		 * printInformation(""); printInformation("/t   /n");
+		 */
+		
+		
+		
+		
+
+	}
+	
+	public static void printInformation(String string) {
+		
+		int length=string.length();
+		if(string.isEmpty()) {
+			System.out.println("The string is empty.");
+		}
+		
+		if(string.isBlank()) {
+			System.out.println("The string is blank.");
+		}
+		System.out.printf("Length =%d %n", length);
+		System.out.printf("First Char =%c %n", string.charAt(0));
+		System.out.printf("Last Char =%c %n", string.charAt(length-1));	
+		System.out.printf("Last index =%d %n", string.lastIndexOf('l'));	
+		System.out.println(string.replaceAll("Hello", "New"));
+		System.out.println(string);
+		
+	}
+
+}
---
+++ AllAboutString/src/MainStringBuilder.java
@@ -0,0 +1,30 @@
+
+public class MainStringBuilder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String string="Hello"+" World";
+		string.concat(" & Goodbye!");
+		
+		StringBuilder strinBuilder=new StringBuilder("Hello World");
+		strinBuilder.append(" & Goodbye!!");
+		printInformation(string);
+		printInformation(strinBuilder);
+		
+
+	}
+	
+	public static void printInformation(String string) {
+		
+		System.out.println("String ="+string);
+		System.out.println("Length = "+string.length());
+	}
+	
+	public static void printInformation(StringBuilder strinBuilder) {
+		
+		System.out.println("String Builder ="+strinBuilder);
+		System.out.println("Length = "+strinBuilder.length());
+	}
+
+}
